63bd0cb7a8d02cecc303a914537bd87220fbe7ef
Son <sonlvph46459@fpt.edu.vn>
2024-03-24 17:07:39 +0700

    Nguoi dung+login

---
+++ connection.php
@@ -0,0 +1,17 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+
+try{
+    $conn= new PDO("mysql:host=$servername;dbname=du-an-1",$username,$password);
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+
+}
+catch(PDOException $e){
+    echo "Connection failed: " . $e->getMessage();
+}
+
+?>
